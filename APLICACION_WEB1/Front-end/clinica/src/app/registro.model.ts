0e3e1183cca1d06b0055b16d7da9ab32988e65b8
sebasio3k <bastianhdezo@gmail.com>
2019-05-10 12:35:06 -0500

    actualizado4
    4

---
+++ APLICACION_WEB1/Front-end/clinica/src/app/registro.model.ts
@@ -0,0 +1,20 @@
+export class registro {
+	
+	constructor(
+		public id : number,
+		public name : string,
+		public ap : string,
+		public am : string,
+		public genere : string,
+		public neighborhood : string,
+		public number : number,
+		public streat : string,
+		public age : number,
+		public phone : number,
+		public email : string,
+
+		) {
+		// code...
+
+	}
+}
