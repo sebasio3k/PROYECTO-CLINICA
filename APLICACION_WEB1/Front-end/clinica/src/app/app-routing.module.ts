69294a94fc203bcaf4adf9d4d22fa8a57e20215a
sebasio3k <bastianhdezo@gmail.com>
2019-05-13 21:20:30 -0500

    enlaces

--- APLICACION_WEB1/Front-end/clinica/src/app/app-routing.module.ts
+++ APLICACION_WEB1/Front-end/clinica/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistroFormComponent } from './registro-form/registro-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'registro',component:RegistroFormComponent},
+  {path:'**',pathMatch:'full',redirectTo:'login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- APLICACION_WEB1/Front-end/clinica/src/app/login/login.component.ts
+++ APLICACION_WEB1/Front-end/clinica/src/app/login/login.component.ts
@@ -6,9 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  pass;
+  usr;
   constructor() { }
-
+  inicioSesion(){
+    if(this.usr=="admin" && this.pass=="1234"){
+      alert("Datos Correctos");
+      document.location.href="registro";
+    }else{
+      alert("Datos incorrectos");
+    }
+  }
   ngOnInit() {
   }
 
